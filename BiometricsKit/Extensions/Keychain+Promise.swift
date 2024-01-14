73ed55131f0d7bc8931534043b2d04cdbb946244
Shane Whitehead <obishiro@gmail.com>
2024-01-14 13:22:15 +1100

    Update to Xcode 15 and SPM

--- BiometricsKit/Extensions/Keychain+Promise.swift
+++ BiometricsKit/Extensions/Keychain+Promise.swift
@@ -11,38 +11,42 @@ import KeychainAccess
 import Hydra
 
 extension Keychain {
-	func authenticate(withPrompt prompt: String, forKey key: String) -> Promise<Data?> {
-		return Promise<Data?>(in: .userInitiated, { (fulfill, fail, _) in
-			let data = try self.authenticationPrompt(prompt).getData(key)
-			fulfill(data)
-		})
-	}
-	
-	func set(_ value: String,
-					 forKey key: String,
-					 withPrompt prompt: String,
-					 accessibility: Accessibility = .whenPasscodeSetThisDeviceOnly,
-					 authenticationPolicy: AuthenticationPolicy = .touchIDCurrentSet) -> Promise<Void> {
-		return Promise<Void>(in: .userInitiated, { (fulfill, fail, _) in
-			try self.accessibility(accessibility,
-														 authenticationPolicy: authenticationPolicy)
-				.authenticationPrompt(prompt)
-				.set(value, key: key)
-			fulfill(())
-		})
-	}
-	
-	func set(_ data: Data,
-					 forKey key: String,
-					 withPrompt prompt: String,
-					 accessibility: Accessibility = .whenPasscodeSetThisDeviceOnly,
-					 authenticationPolicy: AuthenticationPolicy = .touchIDCurrentSet) -> Promise<Void> {
-		return Promise<Void>(in: .userInitiated, { (fulfill, fail, _) in
-			try self.accessibility(accessibility,
-														 authenticationPolicy: authenticationPolicy)
-				.authenticationPrompt(prompt)
-				.set(data, key: key)
-			fulfill(())
-		})
-	}
+    func authenticate(withPrompt prompt: String, forKey key: String) -> Promise<Data?> {
+        return Promise<Data?>(in: .userInitiated, { (fulfill, fail, _) in
+            let data = try self.authenticationPrompt(prompt).getData(key)
+            fulfill(data)
+        })
+    }
+    
+    func set(
+        _ value: String,
+        forKey key: String,
+        withPrompt prompt: String,
+        accessibility: Accessibility = .whenPasscodeSetThisDeviceOnly,
+        authenticationPolicy: AuthenticationPolicy = .biometryCurrentSet
+    ) -> Promise<Void> {
+        return Promise<Void>(in: .userInitiated, { (fulfill, fail, _) in
+            try self.accessibility(accessibility,
+                                   authenticationPolicy: authenticationPolicy)
+            .authenticationPrompt(prompt)
+            .set(value, key: key)
+            fulfill(())
+        })
+    }
+    
+    func set(
+        _ data: Data,
+        forKey key: String,
+        withPrompt prompt: String,
+        accessibility: Accessibility = .whenPasscodeSetThisDeviceOnly,
+        authenticationPolicy: AuthenticationPolicy = .biometryCurrentSet
+    ) -> Promise<Void> {
+        return Promise<Void>(in: .userInitiated, { (fulfill, fail, _) in
+            try self.accessibility(accessibility,
+                                   authenticationPolicy: authenticationPolicy)
+            .authenticationPrompt(prompt)
+            .set(data, key: key)
+            fulfill(())
+        })
+    }
 }
